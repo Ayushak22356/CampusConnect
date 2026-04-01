e0a5024622d1f42040c288a2158487966246e4d6
Ayush Kumar <154447340+Ayushak22356@users.noreply.github.com>
2026-04-01 09:49:31 +0530

    Delete routes directory

--- routes/categories.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate, authorize } = require('../middleware/auth');
-const { getCategories, createCategory } = require('../controllers/categoryController');
-
-router.get('/', getCategories);
-router.post('/', authenticate, authorize('admin'), createCategory);
-
-module.exports = router;
